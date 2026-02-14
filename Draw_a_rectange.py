5f6c916c1431729d8a07d6fd597d9ba832a5e1f8
Aarushthegamemaster <aarushchandra0604@gmail.com>
2026-02-14 09:14:15 -0800

    Code_1

---
+++ Draw_a_rectange.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+display = pygame.display.set_mode((300,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        
+        if event.type == pygame.KEYDOWN:
+            print("A key has been pressed")
